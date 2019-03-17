74d3f17efe86b33fb38a6ca47a42d802b4b10718
marievu <marie.vu@mail.mcgill.ca>
2019-03-17 17:50:16 -0400

    Eventregistration-web

--- EventRegistration-Frontend/src/components/EventRegistration.vue
+++ EventRegistration-Frontend/src/components/EventRegistration.vue
@@ -2,26 +2,34 @@
   <div id="eventregistration">
     <h2>People</h2>
     <table>
+      <tr v-for="person in people" >
+      <td>{{ person.name }}</td>
+      <td>
+        <ul>
+          <li v-for="event in person.events">
+            {{event.name}}
+          </li>
+        </ul>
+      </td>
+  </tr>
       <tr>
-          <td>John</td>
-          <td>Event to attend</td>
-      </tr>
-      <tr>
-          <td>
-              <input type="text" placeholder="Person Name">
-          </td>
-          <td>
-              <button>Create</button>
-          </td>
-      </tr>
+      <td>
+        <input type="text" v-model="newPerson" 
+        placeholder="Person Name">
+      </td>
+      <td>
+        <button @click="createPerson(newPerson)">Create Person</button>
+      </td>
+  </tr>
     </table>
     <p>
-      <span style="color:red">Error: Message text comes here</span>
+     <span v-if="errorPerson" style="color:red">Error: {{errorPerson}} </span>
     </p>
+    
   </div>
 </template>
 
-<script>
+<script src="./registration.js">
 </script>
 
 <style>
